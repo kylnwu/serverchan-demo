9e614ce3caf20b596b679077e36b2cfdb0ea85bd
kylnwu <74576340+kylnwu@users.noreply.github.com>
2023-08-10 18:53:19 +0800

    Update send.py
    用requests库重写的send方法，更简洁，更易读

--- python/send.py
+++ python/send.py
@@ -19,3 +19,27 @@ key = data['SENDKEY']
 
 ret = sc_send('主人服务器宕机了', '第一行\n\n第二行', key)
 print(ret)
+
+
+# 用requests库写的send方法
+import requests
+
+def push_deer(title, massege, key='[SENDKEY]'):
+    url = f'https://sctapi.ftqq.com/{key}.send?'
+    params = {
+        'title': title,
+        'desp': massege,
+        'channel': '18'
+    }
+
+    resp = requests.post(url, params=params)
+    return resp.text
+    
+data = {}
+with open(os.path.join(os.path.dirname(__file__), '..', '.env'), 'r') as f:
+    for line in f:
+        key, value = line.strip().split('=')
+        data[key] = value
+key = data['SENDKEY']
+
+print(push_deer('主人服务器宕机了', "测试测试", key))
